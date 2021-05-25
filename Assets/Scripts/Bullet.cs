02a4a6f1a5d59f38af30785dfaa584f58a07ec06
nagashima0112 <mski4649@icloud.com>
2021-05-25 17:04:07 +0900

    手順の6まで終了 Bullet スクリプトを作成し、ゲーム実行時にバレットを発射する Bullet スクリプトを修正し、左クリックしたときにバレットを発射し、５秒後にはバレットを破壊する。成功したらバレットをプレファブ化する
    ☆新しい学習内容
    ・RequireComponent属
    ・Transform.right 変数
    ・Rigidbody2D.AddForce メソッド
    ・Header属性
    ・Transform.up 変数
    ・Destroy メソッド
    ・Summary（サマリー）機能
    ・プレファブの作成

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// このスクリプトがアタッチしているゲームオブジェクトにアタッチされている Rigidbody2D コンポーネントを取得する
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    [Header("バレットの速度")]
+    public float bulletSpeed;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        // マウスの左ボタンをクリックしたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            // バレットを発射する
+            ShotBullet();
+
+            // Debug.Log で動作を確認
+            Debug.Log("左クリック確認");
+        }
+    }
+
+    /// <summary>
+    /// バレットの制御
+    /// </summary>
+    private void ShotBullet()
+    {
+        // バレットの移動処理
+        // バレットのゲームオブジェクトにアタッチされている Rigidbody2D コンポーネントを取得して、Rigidbody2D クラスの持つ AddForce メソッドを実行する
+        // 発射する方向と速度を変更
+        GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
+
+       // Debug.Logでこの処理が実行されているか確認
+        Debug.Log("発射速度 : " + bulletSpeed);
+
+        // ５秒後にこのスクリプトがアタッチされているゲームオブジェクト(つまり、バレット)を破壊する
+        Destroy(gameObject, 5f);
+    }
+}
