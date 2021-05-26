6f159907bfc73e71e558ea9cfe6ded1b3a7c1e96
nagashima0112 <mski4649@icloud.com>
2021-05-26 09:54:29 +0900

    手順の9まで終了 Canvas内にエネミー用ゲームオブジェクトを配置し、設定する EnemyController スクリプトを作成し、エネミーの制御を行う Transform クラスの扱う、Tranlate メソッドでキャラを移動させる EnamyController スクリプトを修正し、一定地点までエネミーが移動したら破壊する制御を追加する
    新しい学習内容
    ・Transform.Translate メソッド

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider2D))]
+public class EnemyController : MonoBehaviour
+{
+   
+    void Update()
+    {
+
+        // このスクリプトがアタッチしているゲームオブジェクトを徐々に移動する
+        transform.Translate(0, -0.01f, 0);
+
+        // 一定地点までエネミーが移動したら = このゲームオブジェクトの位置が一定値(-1500)を超えたら
+        if (transform.localPosition.y < -1500)
+        {
+
+            //  このスクリプトがアタッチしているゲームオブジェクトを破壊する
+            Destroy(gameObject);
+        }
+    }
+}
