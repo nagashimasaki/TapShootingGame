a55413249996ce2519435a8ca5ce7abe556f085c
nagashima0112 <mski4649@icloud.com>
2021-05-26 10:35:46 +0900

    手順の10まで終了 ☆処理内容 EnemyController スクリプトを修正し、エネミーのコライダーに対しての侵入判定処理を追加する OnTriggerEnter2D メソッド内に tag を使った if 文を書き、侵入したオブジェクトの tag が Bullet なら if 文内の処理を実行する EnemyController スクリプトを修正し、侵入判定の際にエネミーとバレット両方を破壊する処理を追加する
    ☆新しい学習内容
    ・OnTriggerEnter2D メソッド
    ・Tag の設定
    ・処理のメソッド化

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(CapsuleCollider2D))]
 public class EnemyController : MonoBehaviour
 {
-   
+
     void Update()
     {
 
@@ -19,5 +19,39 @@ public class EnemyController : MonoBehaviour
             //  このスクリプトがアタッチしているゲームオブジェクトを破壊する
             Destroy(gameObject);
         }
+    }
+
+    /// <summary>
+    /// 侵入判定
+    /// </summary>
+    /// <param name="col"></param>
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        // 侵入したコライダーのゲームオブジェクトの Tag が Bullet なら　=>　Enemy ゲームオブジェクトのコライダーに、Bullet ゲームオブジェクトのコライダーが侵入していたら
+        if (col.gameObject.tag == "Bullet")
+        {
+
+            // 侵入判定の確認
+            Debug.Log("侵入したオブジェクト名 : " + col.gameObject.name);
+
+            // バレットのゲームオブジェクトを破壊する
+            DestroyObjects(col);
+        }
+    }
+
+    /// <summary>
+    /// バレットとエネミーの破壊処理
+    /// </summary>
+    private void DestroyObjects(Collider2D col)
+    {
+        // 侵入判定の確認
+        Debug.Log("侵入したオブジェクト名 : " + col.gameObject.tag);
+
+        // バレットを破壊する
+        Destroy(col.gameObject);
+
+        // このゲームオブジェクトを破壊する
+        Destroy(gameObject);
     }
 }
