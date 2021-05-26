19aeeafcacb26d4d7aee78205b1fe1ed075fa004
nagashima0112 <mski4649@icloud.com>
2021-05-26 16:42:20 +0900

    手順の12まで終了 ・Canvas内にプレイヤーの拠点用のゲームオブジェクトを配置し、設定する ・DefenseBase スクリプトを作成し、エネミーが侵入した際にエネミーを破壊する処理を実装する ・DefenseBaseSet オブジェクトの子オブジェクトにコライダーの情報を持つオブジェクトを作る
    ☆新しい学習内容
    ・OnTriggerEnter2D メソッド、Tag の復習を行う

---
+++ Assets/Scripts/DefenseBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class DefenseBase : MonoBehaviour
+{
+    [Header("拠点の耐久力")]
+    public int durability;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Enemy")
+        {
+
+            // 耐久力の値を減算する
+            durability -= 10;
+
+            // 減算結果の値を Console ビューに表示して計算されているか確認
+            Debug.Log("残りの耐久力 ; " + durability);
+
+            // エネミーのゲームオブジェクトを破壊する
+            Destroy(col.gameObject);
+        }
+    }
+}
