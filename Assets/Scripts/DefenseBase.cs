605fc4ef0de6c9b1ef0949fbf59077797870639b
nagashima0112 <mski4649@icloud.com>
2021-05-27 22:04:39 +0900

    手順の15まで終了 ☆処理内容 ・DefenseBaseスクリプトを修正し、ゲーム画面に拠点の耐久力の値を表示・更新する処理を実装する ・問題点を見つけ、改善方法を考える
    ☆新しい学習内容
    ・SerializeField属性
    ・アタッチとアサインについて
    ・Mathf.Clamp メソッド

--- Assets/Scripts/DefenseBase.cs
+++ Assets/Scripts/DefenseBase.cs
@@ -1,12 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class DefenseBase : MonoBehaviour
 {
     [Header("拠点の耐久力")]
     public int durability;
+
+    [SerializeField]
+    private Text txtDurability;
+
+    private int maxDurability;     // 耐久力の最大値を代入しておく
+
+    void Start()
+    {
+
+        // ゲーム開始時点の耐久力の値を最大値として代入
+        maxDurability = durability;
+
+        // 耐久力の表示更新
+        DisplayDurability();
+    }
+
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -35,11 +52,31 @@ public class DefenseBase : MonoBehaviour
 
         // 耐久力の値を減算する
         durability -= enemy.attackPower;
+
+        // 耐久力の値を上限・下限値の範囲内に収まるか確認し、それを超えた場合には上限・下限値に置き換えて制限する
+        durability = Mathf.Clamp(durability, 0, maxDurability);
+
         // 減算結果の値を Console ビューに表示して計算されているか確認
         Debug.Log("残りの耐久力 ; " + durability);
 
+        // 耐久力の表示更新
+        DisplayDurability();
+
+        // TODO ゲージの表示を耐久力の値に合わせて更新
         // TODO 耐久力が 0 以下になっていないか確認
         // TODO 耐久力が 0 以下なら、ゲームオーバーの判定を行う
+    }
+
+    /// <summary>
+    /// 耐久力の表示更新
+    /// </summary>
+    private void DisplayDurability()
+    {
+
+        // 画面に耐久力の値を　現在値 / 最大値　の形式で表示する
+        txtDurability.text = durability + "  / " + maxDurability;
+
+        // TODO ゲージの表示を耐久力の値に合わせて更新
     }
 
 }
