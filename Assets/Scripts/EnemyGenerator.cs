1c8315a9efb79de59583b4f7e878fa7dfeeaefbb
nagashima0112 <mski4649@icloud.com>
2021-06-05 12:02:59 +0900

    手順の24まで終了 ☆処理内容 ・EnemyGenerator スクリプトを修正して、生成したエネミーが一定数に達したら生成を終了する制御を追加する ・EnemyGenerator スクリプトを修正して、ボス討伐状態を追加し、ゲーム終了状態に切り替える制御を追加する
    ☆新しい学習内容
    ・Debug.Log メソッドを利用した処理の流れの確認方法
    ・StartCoroutine メソッド

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -18,6 +18,25 @@ public class EnemyGenerator : MonoBehaviour
 
     private GameManager gameManager;
 
+    [Header("エネミーの最大生成数")]
+    public int maxGenerateCount;
+
+    [Header("エネミーの生成完了管理用")]
+    public bool isGenerateEnd;
+
+    [Header("ボス討伐管理用")]
+    public bool isBossDestroyed;
+
+    private void Start()
+    {
+
+        //ゲームスタート時には生成未完了の状態にする
+        isGenerateEnd = false;
+
+        //ゲームスタート時にはボスを未討伐状態にする
+        isBossDestroyed = false;
+    }
+
     /// <summary>
     /// EnemyGenerator の設定
     /// </summary>
@@ -52,6 +71,20 @@ public class EnemyGenerator : MonoBehaviour
             generateCount++;
 
             Debug.Log("生成したエネミーの数 : " + generateCount);
+
+            // 今までに生成したエネミーの数が、エネミーの最大生成数の値と同じか超えたら
+            if (generateCount >= maxGenerateCount)
+            {
+
+                // 生成完了の状態にする 
+                isGenerateEnd = true;
+
+                Debug.Log("生成完了");
+
+                // ボスの生成
+                StartCoroutine(GenerateBoss());
+
+            }
         }
     }
 
@@ -70,6 +103,12 @@ public class EnemyGenerator : MonoBehaviour
     void Update()
     {
 
+        // 生成完了の状態になったら生成処理を行わないようにする
+        if (isGenerateEnd)
+        {
+            return;
+        }
+
         // ゲーム終了の状態ではないなら
         if (!gameManager.isGameUp)
         {     // ! が先頭についているので、gameManager.isGameUp == false と同じ条件式
@@ -78,5 +117,42 @@ public class EnemyGenerator : MonoBehaviour
             // エネミー生成の準備
             PreparateGenerateEnemy();
         }
+    }
+
+    /// <summary>
+    /// ボスの生成
+    /// </summary>
+    private IEnumerator GenerateBoss()
+    {
+
+        // TODO ボス出現の警告演出
+        Debug.Log("ボス出現の警告演出");
+
+
+        yield return new WaitForSeconds(1.0f);
+
+        // TODO ボス生成
+
+        // TODO ボス討伐(仮)
+        SwitchBossDestroyed(true);
+    }
+
+    /// <summary>
+    /// ボス討伐状態の切り替え
+    /// </summary>
+    /// <param name="isSwitch"></param>
+    public void SwitchBossDestroyed(bool isSwitch)
+    {
+        // ボスの討伐状態を切り替え
+        isBossDestroyed = isSwitch;
+
+        Debug.Log("ボス討伐");
+
+        // ボス討伐に合わせて、ゲーム終了の状態に切り替える
+        gameManager.SwitchGameUp(isBossDestroyed);
+
+
+        // TODO ゲームクリアの準備
+
     }
 }
