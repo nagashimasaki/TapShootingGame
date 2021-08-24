da8931c808570cb5a148bb61156e474fa9a2ee52
nagashima0112 <mski4649@icloud.com>
2021-08-24 19:49:31 +0900

    発展の18まで終了
    ☆処理内容
    ・BulletSelectDetail スクリプトを作成する
    ・BulletSelectManager スクリプトを作成し、btnBulletSelect ゲームオブジェクトを自動生成する処理を追加する
    
    ☆新しい学習内容
    ・for 文と Grid Layout Group コンポーネントを利用したインスタンシエイト処理の実装例

---
+++ Assets/Scripts/BulletSelectDetail.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BulletSelectDetail : MonoBehaviour
+{
+    [SerializeField]
+    private Button btnBulletSelect;
+
+    private BulletSelectManager bulletSelectManager;
+
+
+    /// <summary>
+    /// 初期設定
+    /// </summary>
+    /// <param name="bulletSelectManager"></param>
+    public void SetUpBulletSelectDetail(BulletSelectManager bulletSelectManager)
+    {
+
+        this.bulletSelectManager = bulletSelectManager;
+
+        // ボタンにメソッド登録
+        btnBulletSelect.onClick.AddListener(OnClickBulletSelect);
+    }
+
+    /// <summary>
+    /// バレット選択
+    /// </summary>
+    public void OnClickBulletSelect()
+    {
+        Debug.Log("バレット選択");
+    }
+}
---
+++ Assets/Scripts/BulletSelectManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSelectManager : MonoBehaviour
+{
+    [SerializeField]
+    private BulletSelectDetail bulletSelectDetailPrefab;　　// BulletSelectDetail スクリプトがアタッチされている、プレファブの btnBulletSelect ゲームオブジェクトをアサインする
+
+    [SerializeField]
+    private Transform bulletTran;                           // バレット選択ボタンの生成位置として利用するゲームオブジェクトをアサインする
+
+    private const int maxBulletBtnNum = 0;                  // バレット選択ボタンの最大数。定数として宣言する
+
+    public List<BulletSelectDetail> bulletSelectDetailList = new List<BulletSelectDetail>();      // 生成されたバレット選択ボタンの List
+
+
+    void Start()
+    {
+        // バレット選択ボタンの生成
+        StartCoroutine(GenerateBulletSelectDetail());
+    }
+
+    /// <summary>
+    /// バレット選択用ボタンの生成
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator GenerateBulletSelectDetail()
+    {
+
+        // ボタンの最大数を目標値に処理を繰り返す
+        for (int i = 0; i < maxBulletBtnNum; i++)
+        {
+
+            // バレットボタン生成
+            BulletSelectDetail bulletSelectDetail = Instantiate(bulletSelectDetailPrefab, bulletTran, false);
+
+            // バレットボタンの設定
+            bulletSelectDetail.SetUpBulletSelectDetail(this);
+
+            // リストに追加
+            bulletSelectDetailList.Add(bulletSelectDetail);
+
+            // 0.25 秒だけ処理を中断(順番にボタンが生成されるようにする演出)
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+}
