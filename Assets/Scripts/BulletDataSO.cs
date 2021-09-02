ffed4603878af9cae01b3a45625caa5f758fef37
nagashima0112 <mski4649@icloud.com>
2021-09-02 11:26:34 +0900

    発展の20まで終了
    ☆処理内容
    ・BulletDataSO スクリプトを作成する
    ・BulletDataSO スクリプトを利用して BulletDataSO スクリプタブル・オブジェクトを作成し、バレットのデータを登録する
    
    ☆新しい学習内容
    ・クラス内に入れ子として enum を宣言して利用する方法

---
+++ Assets/Scripts/BulletDataSO.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "BulletDataSO", menuName = "Create BulletDataSO")]
+public class BulletDataSO : ScriptableObject
+{
+
+    public List<BulletData> bulletDataList = new List<BulletData>();
+
+    /// <summary>
+    /// バレットの種類
+    /// </summary>
+    [Serializable]
+    public enum BulletType
+    {
+        A,
+        B,
+        C,
+        Player_Normal,
+        Player_Blaze,
+        Player_3ways_Piercing,
+        Player_5ways_Normal,
+        None
+    }
+
+    /// <summary>
+    /// 利用者の種類
+    /// </summary>
+    [Serializable]
+    public enum LiberalType
+    {
+        Player,
+        Enemy,
+        BOSS
+    }
+
+    /// <summary>
+    /// バレット用のデータ
+    /// </summary>
+    [Serializable]
+    public class BulletData
+    {
+        public int no;　　　　　　　　　　　 // バレットの通し番号
+        public float bulletSpeed;　　　　　　// 速度
+        public int bulletPower;　　　　　　　// 攻撃力
+        public float loadingTime;　　　　　　// 発射までの待機時間
+        public BulletType bulletType;　　　　// バレットの種類
+        public Sprite btnSprite;　　　　　　 // バレット選択用のボタンの画像
+        public LiberalType liberalType;　　　// バレットの利用者の種類
+        public int openExp;　　　　　　　　　// バレットを使用するために必要な Exp
+        public float launchTime;　　　　　　 // バレットを使用できる時間
+        public Sprite bulletSprite;　　　　　// 発射するバレットの画像
+        public string discription;　　　　　 // バレットの説明文
+    }
+}
